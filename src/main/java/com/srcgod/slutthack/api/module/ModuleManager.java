7aab3de4322fffd00d71f735d0175c2c9c9e98bf
sledk <srcgod>
2022-06-02 19:50:34 +0300

    цвыы

--- src/main/java/com/srcgod/slutthack/api/module/ModuleManager.java
+++ src/main/java/com/srcgod/slutthack/api/module/ModuleManager.java
@@ -1,6 +1,7 @@
 package com.srcgod.slutthack.api.module;
 
 import com.srcgod.slutthack.impl.Client;
+import com.srcgod.slutthack.impl.modules.combat.TriggerBot;
 import com.srcgod.slutthack.impl.modules.movement.Fly;
 import com.srcgod.slutthack.impl.modules.player.Blockreach;
 
@@ -9,6 +10,7 @@ public class ModuleManager {
     public static void init() {
         register(new Fly());
         register(new Blockreach());
+        register(new TriggerBot());
     }
 
     private static void register(Module module) {
